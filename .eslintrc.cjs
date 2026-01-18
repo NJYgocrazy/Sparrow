8adfe6173f3f7377ca9012f4cd26f1899dc022b2
NJYgocrazy <3312584336@qq.com>
2026-01-19 01:25:51 +0800

    chore: init local env

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+// .eslintrc.cjs
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": 0,
+  },
+};
